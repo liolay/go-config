7f3341743c6acdbed578c067dae8b3519392f303
liolay <565055991@qq.com>
2018-05-07 15:53:19 +0800

    pre-release

--- client/client.go
+++ client/client.go
@@ -7,29 +7,123 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"os"
+	"fmt"
+	"path/filepath"
+	"bytes"
+	"flag"
 )
+
 type ConfigInfo struct {
 	Name       string
 	Content    []byte
 	UpdateTime time.Time
 }
 
+var (
+	homePaths []string
+	server    string
+	tick      int
+	force     bool
+)
+
+func init() {
+	commandHomePath := flag.String("home", os.Getenv("HOME")+"/.diamond-client", "dirs to store config files from server,multi dirs use comma split")
+	commandServer := flag.String("server", "http://localhost:5337", "server to fetch config files")
+	commandTick := flag.Int("tick", 15, "interval second between two fetch request")
+	commandForce := flag.Bool("force", false, "whether or not force pull all config files regardless changes")
+	flag.Parse()
+
+	homePaths = strings.Split(*commandHomePath, ",")
+	server = *commandServer
+	tick = *commandTick
+	force = *commandForce
+}
 
 func main() {
+	for range time.Tick(time.Duration(tick) * time.Second) {
+		for _, homePath := range homePaths {
+			fmt.Println(homePath)
+			configInfos, err := fetch(homePath)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 
-	for range time.Tick(5 * time.Second) {
-		reader := strings.NewReader("")
-		resp,_ := http.Post("http://localhost:6973/refresh","application/json;charset=utf-8",reader)
-		defer resp.Body.Close()
-
-		responseBody, _ := ioutil.ReadAll(resp.Body)
-
-		var configInfos []ConfigInfo
-		json.Unmarshal(responseBody, &configInfos)
-
-		for _, config := range configInfos {
-			ioutil.WriteFile(config.Name+"_new",config.Content,os.ModePerm)
+			fmt.Println(configInfos)
+			createFile(configInfos, homePath)
 		}
 	}
 
+}
+
+func createFile(configInfos []ConfigInfo, configRoot string) {
+	for _, config := range configInfos {
+		filePath := configRoot + config.Name
+		createParentDir(filePath)
+
+		configFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		defer configFile.Close()
+		configFile.Write(config.Content)
+		os.Chtimes(filePath, config.UpdateTime, config.UpdateTime)
+	}
+}
+
+func fetch(configRoot string) ([]ConfigInfo, error) {
+	buffer := bytes.NewBufferString("")
+	if !force {
+		fileStateBytes, e := json.Marshal(fds(configRoot))
+		if e != nil {
+			fmt.Println(e)
+		}
+		buffer = bytes.NewBuffer(fileStateBytes)
+	}
+	resp, err := http.Post(server+"/sync", "application/json;charset=utf-8", buffer)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var configInfos []ConfigInfo
+	err = json.Unmarshal(responseBody, &configInfos)
+	return configInfos, err
+}
+
+func fds(rootPath string) map[string]time.Time {
+	fds := make(map[string]time.Time)
+
+	filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		if info == nil || info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			fmt.Print(err)
+			return err
+		}
+
+		fds[strings.Replace(path, rootPath, "", 1)] = info.ModTime()
+
+		return nil
+	})
+
+	return fds
+}
+
+func createParentDir(filePath string) {
+	parentPath := string([]rune(filePath)[:strings.LastIndex(filePath, "/")])
+	if _, err := os.Stat(parentPath); os.IsNotExist(err) {
+		err = os.MkdirAll(parentPath, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -8,12 +8,11 @@ import (
 	"log"
 	"path/filepath"
 	"io/ioutil"
+	"strings"
+	"flag"
+	"bufio"
+	"strconv"
 )
-
-type Param struct {
-	folder string
-	stat   map[string]time.Time
-}
 
 type ConfigInfo struct {
 	Name       string
@@ -21,41 +20,81 @@ type ConfigInfo struct {
 	UpdateTime time.Time
 }
 
+var (
+	homePath string
+	repo     string
+	port     string
+)
+
+func init() {
+	commandHomePath := flag.String("home", os.Getenv("HOME"), "config server home path")
+	commandRepo := flag.String("repo", "config-repo", "config server repo")
+	commandPort := flag.Int("port", 5337, "config server running port")
+	flag.Parse()
+
+	homePath = strings.TrimSuffix(*commandHomePath, "/")
+	repo = "/" + strings.Trim(*commandRepo, "/")
+	port = strconv.Itoa(*commandPort)
+}
+
 func main() {
-	http.HandleFunc("/refresh", func(writer http.ResponseWriter, request *http.Request) {
-		//body := request.Body
-		//if body != nil {
-		//	if jsonString, err := ioutil.ReadAll(body); err == nil {
-		//		var param []Param
-		//		refreshPo := json.Unmarshal(jsonString, &param)
-		//		fmt.Printf("%v", refreshPo)
-		//	}
-		//}
+	http.HandleFunc("/sync", func(writer http.ResponseWriter, request *http.Request) {
+		clientFDS := parseClientFDS(request)
+		var configInfos []ConfigInfo
 
-		writer.Header().Add("Content-Type", "application/json;charset=utf-8");
-		writer.WriteHeader(200)
-
-		configInfos := []ConfigInfo{}
-		serverConfigFolder := os.Getenv("HOME") + "/config/"
-		filepath.Walk(serverConfigFolder, func(path string, info os.FileInfo, err error) error {
-			if info.IsDir() {
+		filepath.Walk(homePath+repo, func(path string, info os.FileInfo, err error) error {
+			if info == nil || info.IsDir() {
 				return nil
 			}
-			bytes, _ := ioutil.ReadFile(path)
+
+			filePath := strings.Replace(path, homePath, "", 1)
+			if !changed(clientFDS, filePath, info.ModTime()) {
+				return nil
+			}
+
+			fileContent, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
 			configInfos = append(configInfos, ConfigInfo{
-				Name:       path,
-				Content:    bytes,
+				Name:       filePath,
+				Content:    fileContent,
 				UpdateTime: info.ModTime(),
 			})
 
 			return nil
 		})
 
-		bytes, _ := json.Marshal(configInfos)
-		os.Stdout.Write(bytes)
-		writer.Write(bytes)
+		repoBytes, _ := json.Marshal(configInfos)
+
+		writer.Header().Add("Content-Type", "application/json;charset=utf-8")
+		writer.WriteHeader(200)
+		writer.Write(repoBytes)
 	})
 
-	log.Fatal(http.ListenAndServe(":6973", nil))
+	console := bufio.NewWriter(os.Stdout)
+	console.WriteString("server stated at port:" + port + ",home:" + homePath + ",repo:" + repo)
+	console.Flush()
 
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
+
+func parseClientFDS(request *http.Request) map[string]time.Time {
+	var requestFDS map[string]time.Time
+	if request.Body != nil {
+		if jsonString, err := ioutil.ReadAll(request.Body); err == nil {
+			json.Unmarshal(jsonString, &requestFDS)
+		}
+	}
+	return requestFDS
+}
+
+func changed(clientFDS map[string]time.Time, filepath string, modTime time.Time) bool {
+	clientModTime, present := clientFDS[filepath]
+	if present {
+		return !clientModTime.Equal(modTime)
+	}
+	return true
 }
